6e5ab4a728fbc2f0dfc595a744fa19f516e48a6d
kenanboylu <kenanboylu@gmail.com>
2021-12-31 08:47:26 +0300

    Unit Test merge
    Unit Test merge

--- leave/src/test/java/com/restapi/leave/service/test/LeaveServiceTest.java
+++ leave/src/test/java/com/restapi/leave/service/test/LeaveServiceTest.java
@@ -1,4 +1,4 @@
-package com.restapi.leave1.service;
+package com.restapi.leave.service.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
